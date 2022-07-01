07d313dc4d21c15861ca49e9f065d234fd0d8694
Emmanuel Aaron Popa Campusano <emmanuelaaronpopa@gmail.com>
2022-06-30 22:37:47 -0400

    First Commit

---
+++ arary.js
@@ -0,0 +1,76 @@
+let array = [1,2, 3, 4, 5, 6, 7, 8];
+
+/* function sum(a,b,...c){
+    let result = a + b;
+    for (let i = 0; i < c.length; i++) {
+        const element = c[i];
+        result += element;      
+    } 
+    return result;
+}
+
+console.log(sum(...array));
+ */
+
+
+/* 
+//!Accessing the array
+console.log(array[1]);
+
+console.log("================================")
+
+//!Longitude of the object
+console.log(array.length);
+
+console.log("================================")
+
+//!Accessing the last value of the array
+console.log(array[array.length - 1]);
+
+console.log("================================")
+
+// ! Accediendo al valor del array (Esta opcion es parecida a la 
+// de los corchetes con la diferencia que este permite numeros negativos)
+console.log(array.flat(-1));
+
+console.log("================================")
+
+//!Accessing the array with loop for
+for(let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+
+console.log("================================")
+
+//!Imprimiendo los nuemeros desde el ultimo hasta el primero.
+for (let i = array.length - 1; i >= 0; i--) {
+    console.log(array[i]);  
+}
+
+console.log("================================") */
+
+// !For in a function
+
+/* show(array)
+function show(arr){
+    for (let i = array.length - 1; i >= 0; i--) {
+        console.log(array[i]);  
+    }
+} */
+
+/* console.log([1, 10, 2, 21].sort((a, b) => b - a))
+
+
+function sum(a,b){
+    return a + b;
+}
+
+const sum2 = (a,b) => a + b;
+ */
+/* 
+( () => console.log('Holas'))();
+ */
+
+
+Map(array)
+
---
+++ javascript.js
@@ -0,0 +1,129 @@
+/* 
+let name = " Emmanuel";
+let lastName = " Popa"
+//let text = "klk".concat(name,lastName)
+
+document.getElementById('example').innerText = name;
+
+  */
+/* 
+question = prompt("Cual es la capital de francia:");
+
+res = question.trim();
+
+console.log(res)
+ */
+
+/* const funcion = function(x, y){
+
+    console.log(x)
+    console.log(y)
+
+    if(x === y){
+        console.log('Son iguales')
+
+    }else{
+        console.log('son diferenmtes')
+    }
+}
+
+funcion(2,5,99) */
+
+/* const example = function(x,y){
+    if (x == 'si'){
+        document.getElementById('example').style.fontSize = '200px';
+
+    }else if (x == 'no') {
+        document.getElementsByTagName('body').style.fontSize = '10px';
+        
+    }
+};
+
+y = 'Emmanuel Popa';
+x = prompt('Quieres una fuente grande?');
+
+document.getElementById('example').innerHTML = y;
+document.getElementsByTagName(button).
+example(x) */
+
+/* 
+const notas = [89, 100, 88, 78, 60];
+
+let acumulador = 0
+
+for (let i = 0; i < notas.length; i++){
+    acumulador += notas[i] 
+}
+
+
+let promedio = acumulador / notas.length;
+console.log(promedio)
+
+let funcion = "es un bloque de codigo al cual le agregamos funcionalidades con el objetivo de que sean reutilizadas en lo adelante"; */
+
+/* const notas = [89, 100, 88, 78, 60];
+const frutas = ['mango', 'pera', 'pi;a', 'limon'] 
+const precios = [30.00, 35.00, 50.00, 15.00]
+
+
+const imprimerArreglo = function(arreglo, arreglo) {        
+    for (let i = 0; i < arreglo.length; i++){
+        console.log(arreglo[i]) 
+    }
+}
+
+imprimerArreglo(notas, frutas) */
+
+/* 
+let validacion = function(){
+    let edad = document.getElementById("edad").value;
+
+    if(edad < 18){
+        alert('Eres menor de edad')
+    }else if(edad >= 18){
+        alert('Eres mayor')
+    }
+}
+ */
+
+/* let arr = ['enero', 'febrero', 'marzo', 'abril', 'junio', 'julio', 'etc..'];
+
+console.log(arr) */
+/* 
+let num1 = prompt('Ingrese el primer numero:');
+let num2 = prompt('Ingrese el segundo numero:');
+
+if (num1 > num2){
+    console.log(`El numero ${num1} es mayor que ${num2}`)
+}else if(num1 == num2){
+    console.log(`El numero ${num1} es igual a ${num2}`)
+}else if (num1 < num2){
+    console.log(`El numero ${num1} es menor que ${num2}`)
+}else if(num1 == isNaN){
+    console.log('No es un numero')
+}
+
+ */
+/* 
+const fruits = ["Apple", "Banana", "Orange"];
+
+for (x of fruits) {
+  console.log(x);
+} */
+
+/* for (i = 0; i < 10; i++) {
+    if (i == 5) {
+       continue;
+    }
+    console.log(i);
+  }
+ */
+
+  /* document.getElementById("demo").addEventListener("click", myFunction()); */
+/* 
+  let lol = {
+    firt: "lo",
+    name: 'lol'
+  }
+  console.log(typeof lol)
+ */
